2a23e100b339660fb3ff2e06ba55867b8e2569a4
Vinicius Lagêdo <viniciuslagedo@gmail.com>
2017-11-17 01:44:31 -0200

    Add templatecache task for html templates

--- gulp-tasks/html.js
+++ gulp-tasks/html.js
@@ -2,12 +2,16 @@
 
 const gulp = require('gulp'),
     config = require('./config'),
-    htmlmin = require('gulp-htmlmin');
+    htmlmin = require('gulp-htmlmin'),
+    templateCache = require('gulp-angular-templatecache');
 
 // Task To move and minify html files
 gulp.task('html', () => {
     return gulp
         .src([config.assets.htmls, '!**/.index.html'])
         .pipe(htmlmin({collapseWhitespace: true, minifyCSS: true}))
+        .pipe(templateCache('ng-accessibility-bar.templates.js', {
+            module: 'ng-accessibility-bar'
+        }))
         .pipe(gulp.dest(global.pathDestination.path));
 });
